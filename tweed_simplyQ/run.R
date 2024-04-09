961392d3f2a44384d644b3e7c368a9196d82944f
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2024-04-09 11:11:48 +0100

    Somewhat surprisingly, now seems to run for UpperTweed!

--- tweed_simplyQ/run.R
+++ tweed_simplyQ/run.R
@@ -1,13 +1,14 @@
 
 
 library('Rcpp')
-setwd('Mobius/RWrapper')
+setwd('tweed_simplyQ')
 sourceCpp('mobius_r.cpp')
 
 none <- vector(mode='character', length=0) #It would be nice to use NULL or NA instead, but that does not interface well with C++ it seems
 
 
-mobius_setup_from_parameter_and_input_file('../Applications/SimplyP/Tarland/TarlandParameters_v0-3.dat', '../Applications/SimplyP/Tarland/TarlandInputs.dat')
+mobius_setup_from_parameter_and_input_file('BasinObs_upper_testparameters.dat',
+                                           'BasinObs_upper_datainputs.dat')
 
 # alternatively, read your input data in from any source:
 # airt<-read_it_in_from_whatever_format_you_want()
@@ -17,20 +18,25 @@ mobius_setup_from_parameter_and_input_file('../Applications/SimplyP/Tarland/Tarl
 
 
 # This does not modify the global dataset and is thread safe:
-df<-mobius_run_with(c('PET multiplication factor', 'Baseflow index'), list(none, none), c(2.0, 0.7), c('Reach flow (daily mean, cumecs)', 'Agricultural soil water flow'), list(c('Coull'), c('Coull')))
+df<-mobius_run_with(c('PET multiplication factor', 'Baseflow index'),
+                    list(none, none),
+                    c(2.0, 0.7),
+                    c('Reach flow (daily mean, cumecs)', 'Reach flow (daily mean, mm/day)'),
+                    list(c('UpperTweed'), c('UpperTweed')))
 reachflow<-df[[1]]
 soilflow<-df[[2]]
 
 
 
 # This does modify the global dataset and is NOT thread safe:
-mobius_set_parameter_time('Start date', none, '2003-01-01')
-mobius_set_parameter_uint('Timesteps', none, 1000)
+mobius_set_parameter_time('Start date', none, '1994-01-01')
+mobius_set_parameter_time('End date', none, '2000-12-31')
+#mobius_set_parameter_uint('Timesteps', none, 2557)
 mobius_run_model()
-flow <- mobius_get_result_series('Reach flow (daily mean, cumecs)', c('Coull'))
+flow <- mobius_get_result_series('Reach flow (daily mean, cumecs)', c('UpperTweed'))
 plot(flow, type='l', col='blue')
 
 mobius_set_parameter_double('PET multiplication factor', none, 5.0)
 mobius_run_model()
-flow2 <- mobius_get_result_series('Reach flow (daily mean, cumecs)', c('Coull'))
+flow2 <- mobius_get_result_series('Reach flow (daily mean, cumecs)', c('UpperTweed'))
 lines(flow2, type='l', col='red')
