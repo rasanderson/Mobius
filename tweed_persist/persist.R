bbd081f2196dcae47bd3a018806b6c83a201d778
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2024-07-26 15:22:40 +0100

    Direct edits of reach length and catchment area inside R

--- tweed_persist/persist.R
+++ tweed_persist/persist.R
@@ -1,51 +1,41 @@
-
+# Basic R interface to PERSiST using external CSV data file
 
 library('Rcpp')
-
 sourceCpp('mobius_r.cpp')
 
-none <- vector(mode='character', length=0) # It would be nice to use NULL or NA
+# Keep next few lines as needed for some C++ calls.
+none0 <- vector(mode='character', length=0) # It would be nice to use NULL or NA
                                            # instead, but that does not
                                            # interface well with C++ it seems
+none1 <- vector(mode='character', length=1)
+
+# Read from standard CSV
+upper_tweed <- read.csv("temp_rain_Q_upper.csv", header=TRUE) 
+upper_tweed$date <- as.Date(upper_tweed$date, format="%Y-%m-%d")
+catchment_area <- 1800
+
+cubic_metres_per_day <- upper_tweed$Q * 86400
+flow_mm_per_day <- cubic_metres_per_day / catchment_area
+
+mobius_setup_from_parameter_file_and_input_series('BasinObs_upper_testparameters.dat',
+                                                 InputDataStartDate = as.character(upper_tweed$date[1]),
+                                                 AirTemperature = upper_tweed$temperature,
+                                                 Precipitation = upper_tweed$precipitation)
+
+mobius_set_parameter_double('Terrestrial catchment area', 'UpperTweed', catchment_area)
+mobius_set_parameter_uint('Timesteps', none0, length(upper_tweed$date))
+mobius_set_parameter_double('Reach length', 'UpperTweed', 80000)
 
 
-mobius_setup_from_parameter_and_input_file('BasinObs_upper_testparameters.dat',
-                                           'BasinObs_upper_datainputs.dat')
-
-# alternatively, read your input data in from any source:
-# airt<-read_it_in_from_whatever_format_you_want()
-# precip<-read_this_in_from_whatever_format_too()
-# input_data_start_date<-'1999-01-01'
-# mobius_setup_from_parameter_file_and_input_series('../Applications/SimplyP/Tarland/TarlandParameters_v0-3.dat', input_data_start_date, airt, precip)
-
-
-# This does not modify the global dataset and is thread safe:
-df<-mobius_run_with(c('Evapotranspiration adjustment', 'Baseflow index'),
-                    list(none, none),
-                    c(2.0, 0.7),
-                    c('Reach flow (daily mean, cumecs)', 'Reach flow (daily mean, mm/day)'),
-                    list(c('UpperTweed'), c('UpperTweed')))
-reachflow<-df[[1]]
-soilflow<-df[[2]]
-
-
-
-# This does modify the global dataset and is NOT thread safe:
-mobius_set_parameter_time('Start date', none, '1994-01-01')
-#mobius_set_parameter_time('End date', none, '2000-12-31')
-mobius_set_parameter_uint('Timesteps', none, 2557)
+# Run the model
 mobius_run_model()
 
-# This is the predicted flow. No idea how to get the observed flow!
-flow <- mobius_get_result_series('Reach flow (daily mean)', c('UpperTweed'))
-plot(flow, type='l', col='blue')
+# This is the predicted flow. Q is observed
+pred_flow <- mobius_get_result_series('Reach flow (daily mean)', c('UpperTweed'))
+plot(upper_tweed$Q, type='l', col='blue')
+lines(pred_flow, type='l', col='red')
 
 # Evapotraspiration
 evap <- mobius_get_result_series('Evapotranspiration X3', c('All'))
 plot(evap, type='l', col='blue')
 
-
-#mobius_set_parameter_double('PET multiplication factor', none, 5.0)
-#mobius_run_model()
-#flow2 <- mobius_get_result_series('Reach flow (daily mean, cumecs)', c('UpperTweed'))
-#lines(flow2, type='l', col='red')
