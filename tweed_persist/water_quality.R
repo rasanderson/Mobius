3a138059b7e4a27d7d373a41dae166c4885d95a9
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2024-12-02 11:55:47 +0000

    Improved the initial screening and wrangling of data

--- tweed_persist/water_quality.R
+++ tweed_persist/water_quality.R
@@ -9,35 +9,83 @@
 library(readxl)
 library(ggplot2)
 library(zoo)
+library(lubridate)
+
+rm(list = ls())
 
 raw_wq <- read_xlsx("tweed_persist/2022 Chemistry Data.xlsx",
                     sheet = "Sheet2")
+raw_wq$`Date Taken` <- as.Date(raw_wq$`Date Taken`)
 raw_upper <- raw_wq[raw_wq$Description == "TWEED ABOVE GALA WATER FOOT",]
 raw_lower <- raw_wq[raw_wq$Description == "River Tweed @ Norham Gauge",]
-raw_upper_TP <- raw_upper[raw_upper$Determinand == "Total Phosphorus (as P) (mg/L)",]
-raw_upper_Nitrate <- raw_upper[raw_upper$Determinand == "Nitrate (as N) (mg/L)",]
-#raw_upper_sediment <- raw_upper[raw_upper$Determinand == "Total solids (dissolved + suspended) (105\u00B0C) (mg/L)", ] # \u00B0 is degrees symbol
-raw_lower_TP <- raw_lower[raw_lower$Determinand == "Total Phosphorus (as P) (mg/L)",]
-raw_lower_Nitrate <- raw_lower[raw_lower$Determinand == "Nitrate (as N) (mg/L)",]
-raw_upper_Nitrate <- raw_upper_Nitrate[, c("Date Taken", "Result")]
+raw_upper_nitrate    <- raw_upper[raw_upper$Determinand == "Nitrate (as N) (mg/L)",]
+raw_lower_nitrate    <- raw_lower[raw_lower$Determinand == "Nitrate (as N) (mg/L)",]
+raw_upper_tot_phosph <- raw_upper[raw_upper$Determinand == "Total Phosphorus (as P) (mg/L)",]
+raw_lower_tot_phosph <- raw_lower[raw_lower$Determinand == "Total Phosphorus (as P) (mg/L)",]
+upper_nitrate    <- raw_upper_nitrate[, c("Date Taken", "Determinand", "Result")]
+lower_nitrate    <- raw_lower_nitrate[, c("Date Taken", "Determinand", "Result")]
+upper_tot_phosph <- raw_upper_nitrate[, c("Date Taken", "Determinand", "Result")]
+lower_tot_phosph <- raw_lower_nitrate[, c("Date Taken", "Determinand", "Result")]
+colnames(upper_nitrate)    <- c("samp_date", "determinand", "result")
+colnames(lower_nitrate)    <- c("samp_date", "determinand", "result")
+colnames(upper_tot_phosph) <- c("samp_date", "determinand", "result")
+colnames(lower_tot_phosph) <- c("samp_date", "determinand", "result")
+rm(list = c("raw_wq", "raw_upper", "raw_lower",
+            "raw_upper_nitrate", "raw_lower_nitrate",
+            "raw_upper_tot_phosph", "raw_lower_tot_phosph"))
 
-colnames(raw_wq_TN) <- c("samp_date", "tot_nitrogen")
-colnames(raw_wq_TP) <- c("samp_date", "tot_phosphorus")
-
-raw_wq <- merge(raw_wq_TN, raw_wq_TP, by = "samp_date")
-# Quite a lot of missing values before 1995
-raw_wq$samp_date <- as.Date(raw_wq$samp_date)
-raw_wq <- raw_wq[raw_wq$samp_date >= as.Date("1995-01-01"),]
-
-plot_data <- raw_upper_Nitrate
-plot_data$moving_avg <- rollmean(plot_data$Result, k = 5, fill = NA, align = "right")
-ggplot(plot_data, aes(x = `Date Taken`, y = Result)) +
+# Check original data all dates
+plot_data <- upper_nitrate
+plot_data$moving_avg <- rollmean(plot_data$result, k = 5, fill = NA, align = "right")
+ggplot(plot_data, aes(x = samp_date, y = result)) +
   geom_point() +
   geom_line(aes(y = moving_avg), colour = "blue")
-ggplot(raw_wq, aes(x = samp_date, y = tot_phosphorus)) +
+plot_data <- lower_nitrate
+plot_data$moving_avg <- rollmean(plot_data$result, k = 5, fill = NA, align = "right")
+ggplot(plot_data, aes(x = samp_date, y = result)) +
   geom_point() +
-  geom_line()
+  geom_line(aes(y = moving_avg), colour = "blue")
+plot_data <- upper_tot_phosph
+plot_data$moving_avg <- rollmean(plot_data$result, k = 5, fill = NA, align = "right")
+ggplot(plot_data, aes(x = samp_date, y = result)) +
+  geom_point() +
+  geom_line(aes(y = moving_avg), colour = "blue")
+plot_data <- lower_tot_phosph
+plot_data$moving_avg <- rollmean(plot_data$result, k = 5, fill = NA, align = "right")
+ggplot(plot_data, aes(x = samp_date, y = result)) +
+  geom_point() +
+  geom_line(aes(y = moving_avg), colour = "blue")
 
+# Both upper and lower Tweed are from 1993, but there is a gap in upper Tweed
+# records between November 2001 and December 2004 inclusive. To allow comparison
+# of both N and P we'll run initial models on Jan 2005 to Dec 2019 data.
+upper_nitrate <- upper_nitrate[upper_nitrate$samp_date >= as.Date("2005-01-01"),]
+lower_nitrate <- lower_nitrate[lower_nitrate$samp_date >= as.Date("2005-01-01"),]
+upper_tot_phosph <- upper_tot_phosph[upper_tot_phosph$samp_date >= as.Date("2005-01-01"),]
+lower_tot_phosph <- lower_tot_phosph[lower_tot_phosph$samp_date >= as.Date("2005-01-01"),]
+
+# Re-check plots: all look OK (there are 4 missing values on moving_avg)
+plot_data <- upper_nitrate
+plot_data$moving_avg <- rollmean(plot_data$result, k = 5, fill = NA, align = "right")
+ggplot(plot_data, aes(x = samp_date, y = result)) +
+  geom_point() +
+  geom_line(aes(y = moving_avg), colour = "blue")
+plot_data <- lower_nitrate
+plot_data$moving_avg <- rollmean(plot_data$result, k = 5, fill = NA, align = "right")
+ggplot(plot_data, aes(x = samp_date, y = result)) +
+  geom_point() +
+  geom_line(aes(y = moving_avg), colour = "blue")
+plot_data <- upper_tot_phosph
+plot_data$moving_avg <- rollmean(plot_data$result, k = 5, fill = NA, align = "right")
+ggplot(plot_data, aes(x = samp_date, y = result)) +
+  geom_point() +
+  geom_line(aes(y = moving_avg), colour = "blue")
+plot_data <- lower_tot_phosph
+plot_data$moving_avg <- rollmean(plot_data$result, k = 5, fill = NA, align = "right")
+ggplot(plot_data, aes(x = samp_date, y = result)) +
+  geom_point() +
+  geom_line(aes(y = moving_avg), colour = "blue")
+rm("plot_data")
 
 # As far as I can tell Mobius needs the same timesteps for hydrology as for
 # water quality. We have roughly (!) monthly data for water quality, but daily
