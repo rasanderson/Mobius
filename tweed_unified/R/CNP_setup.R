cf2b8a5228fb6c6787e57a709e8ff0c1e9ceb8c2
rasanderson <roy.sanderson@newcastle.ac.uk>
2025-04-15 15:01:46 +0100

    Setup file completed for CNP plus manual edits of header

--- tweed_unified/R/CNP_setup.R
+++ tweed_unified/R/CNP_setup.R
@@ -5,7 +5,7 @@ library(lubridate)
 QN_datafile <- "tweed_unified/data/simplyQCNP_datainputs.dat"
 
 lois.raw <- read.csv("tweed_unified/data/lois_majorion-nutrient.csv")
-lois.raw <- lois.raw[, c("SITE_NAME", "DATE","Ammonia", "Nitrate",
+lois.raw <- lois.raw[, c("SITE_NAME", "DATE","Ammonia", "Nitrate", "Nitrogen.particulate",
                          "Carbon.organic.dissolved", "Carbon.organic.particulate",
                          "Phosphorus.total", "Phosphorus.total.dissolved")]
 lois.raw <- lois.raw[lois.raw$SITE_NAME == "Tweed at Boleside" | lois.raw$SITE_NAME == "Tweed at Norham",]
@@ -15,6 +15,8 @@ lois.raw$Carbon.organic.dissolved[is.na(lois.raw$Carbon.organic.dissolved)] <- 0
 lois.raw$Carbon.organic.particulate[is.na(lois.raw$Carbon.organic.particulate)] <- 0
 lois.raw$Phosphorus.total[is.na(lois.raw$Phosphorus.total)] <- 0
 lois.raw$Phosphorus.particulate <- lois.raw$Phosphorus.total - lois.raw$Phosphorus.total.dissolved
+# We don't have suspended sediment, so this is an approximation
+lois.raw$Suspended.sediment <- lois.raw$Carbon.organic.particulate + lois.raw$Nitrogen.particulate
 # Convert dates to YYYY-mm-dd, omit hours, but round any hours after 12:00 to
 # subsequent day as a couple of dates with an am and pm reading leading to
 # duplicates
@@ -31,6 +33,7 @@ lois.raw <- lois.raw[order(lois.raw$SITE, lois.raw$DATE), ]
 
 # Now need to append nitrate data with correct headings to input data
 file.copy("tweed_unified/data/BasinObs_upperlower_datainputs.dat", QN_datafile, overwrite = TRUE)
+# Nitrates
 cat("\n\n\"observed NO3\" {\"LowerTweed\"} :\n", file = QN_datafile, append = TRUE)
 tmp <- lois.raw[lois.raw$SITE_NAME == "LowerTweed",]
 tmp <- tmp[, c("DATE", "Nitrate")]
@@ -51,3 +54,50 @@ tmp <- lois.raw[lois.raw$SITE_NAME == "UpperTweed",]
 tmp <- tmp[, c("DATE", "Ammonia")]
 write.table(tmp, sep = "\t\t", file = QN_datafile,
             append = TRUE, col.names = FALSE, row.names = FALSE, quote = FALSE)
+# Suspended sediment (roughly; from N and C)
+cat("\n\n\"observed SS\" {\"LowerTweed\"} :\n", file = QN_datafile, append = TRUE)
+tmp <- lois.raw[lois.raw$SITE_NAME == "LowerTweed",]
+tmp <- tmp[, c("DATE", "Suspended.sediment")]
+write.table(tmp, sep = "\t\t", file = QN_datafile,
+            append = TRUE, col.names = FALSE, row.names = FALSE, quote = FALSE)
+cat("\n\n\"observed SS\" {\"UpperTweed\"} :\n", file = QN_datafile, append = TRUE)
+tmp <- lois.raw[lois.raw$SITE_NAME == "LowerTweed",]
+tmp <- tmp[, c("DATE", "Suspended.sediment")]
+write.table(tmp, sep = "\t\t", file = QN_datafile,
+            append = TRUE, col.names = FALSE, row.names = FALSE, quote = FALSE)
+# Dissolved organic carbon
+cat("\n\n\"observed DOC\" {\"LowerTweed\"} :\n", file = QN_datafile, append = TRUE)
+tmp <- lois.raw[lois.raw$SITE_NAME == "LowerTweed",]
+tmp <- tmp[, c("DATE", "Carbon.organic.dissolved")]
+write.table(tmp, sep = "\t\t", file = QN_datafile,
+            append = TRUE, col.names = FALSE, row.names = FALSE, quote = FALSE)
+cat("\n\n\"observed DOC\" {\"UpperTweed\"} :\n", file = QN_datafile, append = TRUE)
+tmp <- lois.raw[lois.raw$SITE_NAME == "LowerTweed",]
+tmp <- tmp[, c("DATE", "Carbon.organic.dissolved")]
+write.table(tmp, sep = "\t\t", file = QN_datafile,
+            append = TRUE, col.names = FALSE, row.names = FALSE, quote = FALSE)
+# Dissolved phosphorus
+cat("\n\n\"observed TDP\" {\"LowerTweed\"} :\n", file = QN_datafile, append = TRUE)
+tmp <- lois.raw[lois.raw$SITE_NAME == "LowerTweed",]
+tmp <- tmp[, c("DATE", "Phosphorus.total.dissolved")]
+write.table(tmp, sep = "\t\t", file = QN_datafile,
+            append = TRUE, col.names = FALSE, row.names = FALSE, quote = FALSE)
+cat("\n\n\"observed TDP\" {\"UpperTweed\"} :\n", file = QN_datafile, append = TRUE)
+tmp <- lois.raw[lois.raw$SITE_NAME == "LowerTweed",]
+tmp <- tmp[, c("DATE", "Phosphorus.total.dissolved")]
+write.table(tmp, sep = "\t\t", file = QN_datafile,
+            append = TRUE, col.names = FALSE, row.names = FALSE, quote = FALSE)
+# Particulate phosphorus
+cat("\n\n\"observed PP\" {\"LowerTweed\"} :\n", file = QN_datafile, append = TRUE)
+tmp <- lois.raw[lois.raw$SITE_NAME == "LowerTweed",]
+tmp <- tmp[, c("DATE", "Phosphorus.particulate")]
+write.table(tmp, sep = "\t\t", file = QN_datafile,
+            append = TRUE, col.names = FALSE, row.names = FALSE, quote = FALSE)
+cat("\n\n\"observed PP\" {\"UpperTweed\"} :\n", file = QN_datafile, append = TRUE)
+tmp <- lois.raw[lois.raw$SITE_NAME == "LowerTweed",]
+tmp <- tmp[, c("DATE", "Phosphorus.particulate")]
+write.table(tmp, sep = "\t\t", file = QN_datafile,
+            append = TRUE, col.names = FALSE, row.names = FALSE, quote = FALSE)
+
+# Manually edit header of QN_datafile to include additional timeseries names
+# although not clear if this is essential.
